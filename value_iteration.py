a02a981c0e59894432553f1e7fc3c1c69bd63468
yasufumi <yasufumi.taniguchi@gmail.com>
2018-06-01 13:34:20 +0900

    add sample_trajectories method in value_iteration.py

--- value_iteration.py
+++ value_iteration.py
@@ -1,3 +1,6 @@
+import random
+from functools import reduce
+
 import numpy as np
 
 import gym
@@ -40,37 +43,43 @@ class ValueIteration:
             A = compute_action_value(state)
             policy[state] = A
         policy -= policy.max(axis=1, keepdims=True)
+        max_values = np.broadcast_to(policy.max(axis=1, keepdims=True), policy.shape)
+        policy = np.where(policy == max_values, policy, np.NINF)
         policy = np.exp(policy) / np.exp(policy).sum(axis=1, keepdims=True)
         return V, policy
+
+
+def sample_trajectories(env, policy, n_steps, n_samples):
+    goal_count = 0
+    loop_count = 0
+    ignore_states = reduce(
+        np.bitwise_or, [env.desc.flatten() == s for s in (b'H', b'G')]).nonzero()[0].tolist()
+    states = [i for i in range(env.nS) if i not in ignore_states]
+    trajectories = []
+    while goal_count < n_samples:
+        env.reset()
+        state = random.choice(states)
+        env.s = state
+        done = False
+        trajectory = []
+        while not done:
+            action = np.random.multinomial(1, policy[state]).argmax()
+            trajectory.append((state, action))
+            state, reward, done, info = env.step(action)
+
+        len_trajectory = len(trajectory)
+        if n_steps != len_trajectory:
+            trajectory.extend(
+                [(state, action) for _ in range(n_steps - len_trajectory)])
+        if reward == 1.:
+            trajectories.append(trajectory)
+            goal_count += 1
+        loop_count += 1
+    return trajectories
 
 
 if __name__ == '__main__':
     env = gym.make('EasyFrozenLakeEnv-v0')
     V, policy = ValueIteration(env.nS, env.nA, env.P).run(0.99, 1e-5)
 
-    print('Frozen Lake Env{}'.format(env.render('ansi').getvalue()))
-
-    print('Learned Policy')
-    # reshaped_policy = policy.argmax(axis=1).reshape(env.desc.shape)
-    # reshaped_V = V.reshape(env.desc.shape)
-    # num2arrow = ['←', '↓', '→', '↑']
-    # for row, v in zip(reshaped_policy, reshaped_V):
-    #     for state, value in zip(row, v):
-    #         if value:
-    #             print(num2arrow[state], end=' ')
-    #         else:
-    #             print('・', end='')
-    #     print()
-
-    goal_count = 0
-    loop_count = 0
-    while goal_count < 10:
-        state = env.reset()
-        done = False
-        while not done:
-            action = np.random.multinomial(1, policy[state]).argmax()
-            state, reward, done, info = env.step(action)
-        if reward == 1:
-            goal_count += 1
-        loop_count += 1
-    print(f'{goal_count/loop_count}')
+    print(sample_trajectories(env, policy, 10, 10))
